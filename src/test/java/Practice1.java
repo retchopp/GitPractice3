db7c0fdee9c57133a76a864ea8ddfaac3ee6c7c3
recep ozmen <recepozmen@gmail.com>
2023-03-28 06:33:40 +0300

    first commit

---
+++ src/test/java/Practice1.java
@@ -0,0 +1,14 @@
+public class Practice1 {
+
+    public static void main(String[] args) {
+
+        String myName = "Recep";
+        String myname = new String("Ozmen");
+        System.out.println(myname);
+
+    }
+
+
+
+
+}
